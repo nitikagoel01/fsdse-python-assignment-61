75acdf3d6025db2cb321674fc95b32adab9a71b7
nitikagoel01 <nitikagoel.1693@gmail.com>
2017-06-18 05:10:44 +0000

    Done

--- build.py
+++ build.py
@@ -1,2 +1,12 @@
 def solution(list_of_nums):
     """Enter Code Here"""
+    count1=0
+    count2=0
+    for index in list_of_nums:
+        if index%2 == 0:
+            count1 += 1
+        else:
+            count2 += 1
+
+
+    return {"ODD": count2 ,"EVEN": count1}
